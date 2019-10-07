48edc8eddf7962419084176ee2e775b29b4129e3
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-10-07 22:38:40 +0200

    configure simple build

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var through = require('through2');
+
+gulp.task('tsc', function() {
+    var tsProject = ts.createProject('./tsconfig.json');
+
+    var typescript_error_count = 0;
+
+    var tsResult = tsProject.src()
+        .pipe(tsProject({
+            reporter: ts.reporter.longReporter(),
+            error: function () {
+                typescript_error_count++;
+                this.reporter.error.apply(this.reporter, arguments);
+            },
+            finish: function () {
+                this.reporter.finish.apply(this.reporter, arguments);
+            }
+        }));
+
+    return tsResult.js.pipe(gulp.dest('build'))
+        .pipe(through.obj(function (chunk, enc, cb) {
+            if (typescript_error_count) {
+                this.emit("error", "TypeScript compile errors (count:" + typescript_error_count + ")");
+            }
+            cb(null, chunk)
+        }));
+});

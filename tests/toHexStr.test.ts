5305e5238ea07943c70dd461e596cffb781cebad
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-10-08 13:53:44 +0200

    add/remove some tests

--- tests/toHexStr.test.ts
+++ tests/toHexStr.test.ts
@@ -1,4 +1,4 @@
-import {toHexStr} from "../src/modPow";
+import {bigIntHexStr, toHexStr} from "../src/modPow";
 
 describe("toHexStr tests", () => {
 
@@ -6,10 +6,14 @@ describe("toHexStr tests", () => {
 
     it("should NOT trim first zero if value is 0", () => expect(toHexStr([0])).toBe("0"));
 
-    it("should NOT trim non-leading zeros", () => expect(toHexStr([1, 2])).toBe("2001"));
-
     it("should trim leading zero in array",
         () => expect(toHexStr([1, 0])).toBe("1"));
 
-    it("should convert letters", () => expect(toHexStr([1, 0, 9, 10, 11, 12, 13, 14, 15])).toBe("f00e00d00c00b00a009000001"));
+    it("should be ok", () => {
+        const hexStr = "abc567867878cd67987cde8978987897ab7897c878987fff7987f978789f97f7";
+        const n = bigIntHexStr(hexStr);
+        const back = toHexStr(n);
+
+        expect(back).toBe(hexStr);
+    });
 });

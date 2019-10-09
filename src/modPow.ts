22481c5607a4d1fda62a10063ffcc36d41dc0504
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-10-09 11:37:03 +0200

    some performance improvements

--- src/modPow.ts
+++ src/modPow.ts
@@ -6,13 +6,23 @@ export function modPow(xStr: string, e: number, nStr: string): string {
     return toHexStr(r);
 }
 
+function buildNs(n: number[]): Array<number[]> {
+    const res: Array<number[]> = [n];
+    while (res.length < 1025) {
+        n = sum(n, n);
+        res.push(n);
+    }
+    return res;
+}
+
 function modPow2(x: number[], e: number, n: number[]): number[] {
     let a = x.slice();
     let res = bigIntNum(1);
-    const nByPowed2: Array<number[]> = [n];
+    const nByPowed2: Array<number[]> = buildNs(n);
     while (e > 0) {
-        if (e % 2 === 1) res = mod(mult(res, a), n, nByPowed2);
-        a = mod(mult(a, a), n, nByPowed2);
+        if (e % 2 === 1)
+            res = mod(mult(res, a, n, nByPowed2), n, nByPowed2);
+        a = mod(mult(a, a, n, nByPowed2), n, nByPowed2);
         e >>= 1;
     }
     return res;
@@ -20,76 +30,78 @@ function modPow2(x: number[], e: number, n: number[]): number[] {
 
 function mod(x: number[], n: number[], nByPowed2: Array<number[]>): number[] {
     if (less(x, n)) return x;
-    while (!less(x, n)) x = subtract(x, getMaxLessThan(x, nByPowed2));
+    let last = nByPowed2[nByPowed2.length - 1];
+    while (less(last, x)) {
+        last = sum(last, last);
+        nByPowed2.push(last);
+    }
+    let startFrom = nByPowed2.length - 1;
+    while (!less(x, n)) {
+        let maxLessThan = getMaxLessThan(x, nByPowed2, startFrom);
+        x = subtract(x, maxLessThan.value);
+        startFrom = maxLessThan.index - 1;
+    }
     return x;
 }
 
-function getMaxLessThan(x: number[], nByPowed2: Array<number[]>) {
-    let last = nByPowed2[nByPowed2.length - 1];
-    while (less(last, x)) {
-        last = multByNumber(last, 2);
-        nByPowed2.push(last);
-    }
-    let i = nByPowed2.length - 1;
+function getMaxLessThan(x: number[], nByPowed2: Array<number[]>, i: number): { value: number[], index: number } {
     while (i >= 0 && more(nByPowed2[i], x)) i--;
-    return i >= 0 ? nByPowed2[i] : bigIntNum(0);
+    return {value: i >= 0 ? nByPowed2[i] : [0], index: i};
 }
 
 function multByNumber(a: number[], n: number, shift: number = 0): number[] {
+    if (n === 0) return [0];
+    if (n === 1) return a.slice();
+    if (n === 2) return sum(a, a);
     const res: number[] = [];
     for (let i = 0; i < shift; i++) res.push(0);
-    for (let j = 0; j < a.length; j++) res.push(a[j] * n);
-    return normalizeOverflow(res);
+    let carry: number = 0;
+    for (let j = 0; j < a.length || carry > 0; j++) {
+        const value = (j < a.length ? a[j] : 0) * n + carry;
+        carry = Math.floor(value / foundation);
+        res.push(value % foundation);
+    }
+    return res;
 }
 
-function mult(a: number[], b: number[]): number[] {
+function mult(a: number[], b: number[], n: number[], nByPowed2: Array<number[]>): number[] {
     let res = bigIntNum(0);
-    for (let i = 0; i < b.length; i++) res = sum(res, multByNumber(a, b[i], i));
+    for (let i = 0; i < b.length; i++) res = mod(sum(res, multByNumber(a, b[i], i)), n, nByPowed2);
     return res;
 }
 
 function subtract(a: number[], b: number[]): number[] {
-    if (more(b, a)) throw new Error("Negative values are not supported");
-    const res: number[] = [];
-    for (let i = 0; i < b.length; i++) res.push(a[i] - b[i]);
-    for (let j = b.length; j < a.length; j++) res.push(a[j]);
-    return normalizeNegatives(res);
+    if (more(b, a))
+        throw new Error("Negative values are not supported");
+    const res: number[] = a.slice();
+    let carry: number = 0;
+    for (let i = 0; i < b.length || carry > 0; i++) {
+        res[i] -= carry + (i < b.length ? b[i] : 0);
+        carry = res[i] < 0 ? 1 : 0;
+        if (carry) res[i] += foundation;
+    }
+    let idx = res.length;
+    while (idx > 1 && res[idx - 1] === 0) idx--;
+    return res.slice(0, idx);
 }
 
-function sum(a: number[], b: number[]): number[] {
-    const longer = a.length >= b.length ? a : b;
-    const shorter = a.length >= b.length ? b : a;
-    const res: number[] = [];
-    for (let i = 0; i < shorter.length; i++) res.push(a[i] + b[i]);
-    for (let j = shorter.length; j < longer.length; j++) res.push(longer[j]);
-    return normalizeOverflow(res);
-}
-
-function normalizeOverflow(x: number[]): number[] {
-    let extra = 0;
-    for (let i = 0; i < x.length; i++) {
-        const cur = x[i] + extra;
-        extra = Math.floor(cur / foundation);
-        x[i] = cur % foundation;
-    }
-    while (extra > 0) {
-        x.push(extra % foundation);
-        extra = Math.floor(extra / foundation);
-    }
-    return x;
-}
-
-function normalizeNegatives(x: number[]): number[] {
-    for (let i = 0; i < x.length; i++) {
-        if (x[i] < 0) {
-            if (i + 1 == x.length) throw new Error("Negative values are not supported");
-            x[i] += foundation;
-            x[i + 1]--;
-        }
-    }
+function dropLeadingZeros(x: number[]): number[] {
     let idx = x.length;
     while (idx > 1 && x[idx - 1] === 0) idx--;
     return x.slice(0, idx);
+}
+
+function sum(a: number[], b: number[]): number[] {
+    const max = Math.max(a.length, b.length);
+    const res: number[] = a.slice();
+    let carry: number = 0;
+    for (let i = 0; i < max; i++) {
+        res[i] = (i === res.length ? 0 : res[i]) + (i < b.length ? b[i] : 0) + carry;
+        carry = res[i] >= foundation ? 1 : 0;
+        if (carry > 0) res[i] -= foundation;
+    }
+    if (carry) res.push(1);
+    return res;
 }
 
 function less(a: number[], b: number[]): boolean {
@@ -132,7 +144,7 @@ export function bigIntHexStr(hexStr: string): number[] {
         }
         res.push(chunkValue);
     }
-    return normalizeNegatives(res);
+    return dropLeadingZeros(res);
 }
 
 function getInt(chars: string[], idx: number): number {
@@ -152,9 +164,6 @@ export function toHexStr(x: number[]): string {
         }
         res = res.concat(currentPart.reverse());
         currentPart = [];
-
-        //const p0 = p % X2;
-        //res.push(p0 % X1, Math.floor(p0 / X1), Math.floor(p / X2));
     });
     let i = res.length;
     while (i >= 1) {
@@ -172,4 +181,5 @@ function intToHexLetter(x: number): string {
 
 const foundationPow = 6;
 const foundation: number = Math.pow(16, foundationPow);
-const foundationParts: number[] = Array.from({length: foundationPow}).map((x, idx) => Math.pow(16, idx));
+const foundationParts: number[] = [];
+for(let i = 0; i <= foundationPow; i++) foundationParts.push(Math.pow(16, i));

7e8745dcebd91c33d704de46a6863b7bf71d9d3e
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-10-11 13:32:00 +0200

    convert pub_key_n to base 64 and test converting

--- src/verify.ts
+++ src/verify.ts
@@ -2,9 +2,8 @@ import {b64toHex, BigInteger, removeExtraSymbols} from "./jsbnLite";
 import {Sha256} from "./sha256";
 
 export function verify(code: string, hash: string, nStrPubKey: string, ePubKey: number): boolean {
-    const xStr = b64toHex(hash);
-        const x = new BigInteger(xStr);
-        const m = new BigInteger(nStrPubKey);
-        const r = x.modPowInt(ePubKey, m);
-    return  removeExtraSymbols(r.toHexString()) === Sha256.hash(code);
+    const x = new BigInteger(b64toHex(hash));
+    const m = new BigInteger(b64toHex(nStrPubKey));
+    const r = x.modPowInt(ePubKey, m);
+    return removeExtraSymbols(r.toHexString()) === Sha256.hash(code);
 }
--- tests/sha256.test.ts
+++ tests/sha256.test.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import {SHA256} from "crypto-js";
 import {Sha256} from "../src/sha256";
+import {b64toHex} from "../src/jsbnLite";
 
 describe("test sha256", () => {
     it("should build sha256", () => {
@@ -24,6 +25,13 @@ describe("test sha256", () => {
         }
 
         console.log(`AVG: a1:${totalTime1 / 500}, a2:${totalTime2 / 500}`);
+    });
+
+    it("should convert pubKey from b64 to hex string properly", () => {
+        const input = "ztQYwCE5BU7T9CDM5he6rKoabstXRmkzx54zFPZkWbK530dwtLBDeaWBMxHBUT55CYyboR/EZ4efghPi3CoNGfGWezpjko9P6p2EwGArtHEeS4slhu/SpSIFMjG6fdrpRoNuIAMhq1Z+Pr/+HOd1pThFKeGFr2/NhtAg+TXAzaU=";
+        const output = "ced418c02139054ed3f420cce617baacaa1a6ecb57466933c79e3314f66459b2b9df4770b4b04379a5813311c1513e79098c9ba11fc467879f8213e2dc2a0d19f1967b3a63928f4fea9d84c0602bb4711e4b8b2586efd2a522053231ba7ddae946836e200321ab567e3ebffe1ce775a5384529e185af6fcd86d020f935c0cda5";
+
+        expect(b64toHex(input)).toBe(output);
     });
 });
 
